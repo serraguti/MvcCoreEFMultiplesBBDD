0e03894f3a5d827ed3c582c5e319d692674e3534
serraguti <pacoserranox@gmail.com>
2024-02-21 13:10:09 +0100

    Add project files.

---
+++ MvcCoreEFMultiplesBBDD/Controllers/EmpleadosController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreEFMultiplesBBDD.Models;
+using MvcCoreEFMultiplesBBDD.Repositories;
+
+namespace MvcCoreEFMultiplesBBDD.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private RepositoryEmpleados repo;
+
+        public EmpleadosController(RepositoryEmpleados repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<EmpleadoView> empleados =
+                await this.repo.GetEmpleadosAsync();
+            return View(empleados);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            EmpleadoView empleado =
+                await this.repo.FindEmpleadoAsync(id);
+            return View(empleado);
+        }
+    }
+}
---
+++ MvcCoreEFMultiplesBBDD/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using MvcCoreEFMultiplesBBDD.Data;
+using MvcCoreEFMultiplesBBDD.Models;
+
+#region VIEW
+
+//create view v_empleados
+//as
+//	select isnull(EMP.EMP_NO, 0) as EMP_NO,
+//    EMP.APELLIDO, EMP.OFICIO, EMP.SALARIO
+//	, DEPT.DNOMBRE, DEPT.LOC, DEPT.DEPT_NO
+//	from EMP
+//	inner join DEPT
+//	on EMP.DEPT_NO=DEPT.DEPT_NO
+//go
+
+#endregion
+
+namespace MvcCoreEFMultiplesBBDD.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        private HospitalContext context;
+
+        public RepositoryEmpleados(HospitalContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<EmpleadoView>> GetEmpleadosAsync()
+        {
+            var consulta = from datos in this.context.EmpleadosView
+                           select datos;
+            return await consulta.ToListAsync();
+        }
+
+        public async Task<EmpleadoView> FindEmpleadoAsync(int idEmpleado)
+        {
+            var consulta = from datos in this.context.EmpleadosView
+                           where datos.IdEmpleado == idEmpleado
+                           select datos;
+            return await consulta.FirstOrDefaultAsync();
+        }
+    }
+}
